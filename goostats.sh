43fb608c3b39e73f24cc7fa8e23f792aaab90f65
Martin <martinrabey@gmail.com>
2024-09-06 11:50:35 +0800

    add welcome message and collect filename

--- goostats.sh
+++ goostats.sh
@@ -1,3 +1,7 @@
 # load a given file
 #compute the min / max / range of values in the file
+fname=$1
+echo "working with file:"
+echo "welcome to nells stats script"
 
+# compute min / max / range of values in file 
