130ef34ab010725d74abf99f3829d7ee842ebd3f
Martin <martinrabey@gmail.com>
2024-09-06 11:56:35 +0800

    use supplied filename

--- goostats.sh
+++ goostats.sh
@@ -1,7 +1,7 @@
 # load a given file
 #compute the min / max / range of values in the file
 fname=$1
-echo "working with file:"
+echo "working with file: ${fname}"
 echo "welcome to nells stats script"
 
 # compute min / max / range of values in file 
